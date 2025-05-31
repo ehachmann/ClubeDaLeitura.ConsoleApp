002b1285e74f2abe50dace618b83babc604aaeed
ehachmann <ehachmann@hotmail.com>
2025-05-31 15:55:44 -0300

    Add project files.

---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    public abstract class EntidadeBase
+    {
+        public int id;
+
+        public abstract void AtualizarRegistro(EntidadeBase registroAtualizado);
+
+        public abstract string Validar();
+
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,45 @@
+﻿
+namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+{
+    public class TelaPrincipal
+    {
+        private char opcaoEscolhida;
+
+        public void ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|          Clube da Leitura            |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Controle de Amigos");
+            Console.WriteLine("2 - Controle de Caixas");
+            Console.WriteLine("3 - Controle de Revistas");
+            Console.WriteLine("4 - Controle de Empréstimos");
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+            opcaoEscolhida = Console.ReadLine()[0];
+        }
+
+        public TelaBase ObterTela()
+        {
+           /* if (opcaoEscolhida == '1')
+                return telaEquipamento;
+
+            else if (opcaoEscolhida == '2')
+                return telaChamado;
+
+            else if (opcaoEscolhida == '3')
+                return telaFabricante;*/
+
+            return null;
+        }
+    }
+}
+
